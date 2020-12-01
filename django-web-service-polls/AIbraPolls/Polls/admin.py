717b42ad20e141b9be27946f3ac68320c0d4c076
alekseibragin <73076834+alekseibragin@users.noreply.github.com>
2020-12-01 03:13:02 +0300

    Add files via upload

---
+++ django-web-service-polls/AIbraPolls/Polls/admin.py
@@ -0,0 +1,40 @@
+# from django.contrib import admin
+
+# # Register your models here.
+
+# from django.contrib import admin
+
+# from .models import Question
+
+# #admin.site.register(Question)
+
+# from django.contrib import admin
+
+# from .models import Question
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+# admin.site.register(Question, QuestionAdmin)
+
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+admin.site.register(Question, QuestionAdmin)
